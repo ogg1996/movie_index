18ada74f68f6888b50bbcf388803e5701f4d4e7a
ogg1996 <rjsrnr43@naver.com>
2024-10-07 11:54:31 +0900

    TOP 버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,33 @@ import NavBar from "./components/NavBar";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Search from "./pages/Search";
+import smoothscroll from "smoothscroll-polyfill";
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <NavBar />
-      <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/detail/:id"} element={<MovieDetail />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/join"} element={<Join />} />
-        <Route path={"/search"} element={<Search />} />
-      </Routes>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/detail/:id"} element={<MovieDetail />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/join"} element={<Join />} />
+          <Route path={"/search"} element={<Search />} />
+        </Routes>
+      </div>
+      <button
+        className="text-[20px] font-bold text-white 
+        w-[55px] h-[55px] rounded-[50%] bg-black
+        fixed z-[999] right-[20px] bottom-[20px]"
+        onClick={() => {
+          smoothscroll.polyfill();
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      >
+        TOP
+      </button>
+    </>
   );
 }
 
